d08ea215317d5123f2572b0f943ce826be73aa32
MoustafaC <moustafa.chamli@gmail.com>
2015-01-25 01:49:01 -0500

    progress on az import in godot
    Former-commit-id: d402abd08b2c9aac7c08c17956ef647c284a4425

--- godot/scripts/player.gd
+++ godot/scripts/player.gd
@@ -10,8 +10,7 @@ extends RigidBody
 const ANIM_FLOOR = 0
 const ANIM_AIR_UP = 1
 const ANIM_AIR_DOWN = 2
-
-const CHAR_SCALE = Vector3(0.3,0.3,0.3)
+#const CHAR_SCALE = Vector3(0.3,0.3,0.3)
 
 var facing_dir = Vector3(1, 0, 0)
 var movement_dir = Vector3()
@@ -108,7 +107,6 @@ func _integrate_forces( state ):
 		dir+=cam_xform.basis[0]
 
 	var jump_attempt = Input.is_action_pressed("jump")
-	var shoot_attempt = Input.is_action_pressed("shoot")
 
 	var target_dir = (dir - up*dir.dot(up)).normalized()
 
@@ -139,18 +137,18 @@ func _integrate_forces( state ):
 
 		hv = hdir*hspeed
 
-		var mesh_xform = get_node("Armature").get_transform()
+		var mesh_xform = get_node("armature").get_transform()
 		var facing_mesh=-mesh_xform.basis[0].normalized()
 		facing_mesh = (facing_mesh - up*facing_mesh.dot(up)).normalized()
 		facing_mesh = adjust_facing(facing_mesh,target_dir,delta,1.0/hspeed*turn_speed,up)
-		var m3 = Matrix3(-facing_mesh,up,-facing_mesh.cross(up).normalized()).scaled( CHAR_SCALE )
+		var m3 = Matrix3(-facing_mesh,up,-facing_mesh.cross(up).normalized()) #.scaled( CHAR_SCALE )
 
-		get_node("Armature").set_transform(Transform(m3,mesh_xform.origin))
+		get_node("armature").set_transform(Transform(m3,mesh_xform.origin))
 
 		if (not jumping and jump_attempt):
 			vv = 7.0
 			jumping = true
-			get_node("sfx").play("jump")
+			#get_node("sfx").play("jump")
 	else:
 
 		if (vv>0):
@@ -206,7 +204,7 @@ func _integrate_forces( state ):
 		get_node("AnimationTreePlayer").blend2_node_set_amount("walk",hspeed / max_speed)
 
 	get_node("AnimationTreePlayer").transition_node_set_current("state",anim)
-	#get_node("AnimationTreePlayer").blend2_node_set_amount("gun",min(shoot_blend,1.0))
+#	get_node("AnimationTreePlayer").blend2_node_set_amount("gun",min(shoot_blend,1.0))
 #	state.set_angular_velocity(Vector3())
 
 
