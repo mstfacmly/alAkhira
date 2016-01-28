2d0f6483c48fd09128fc2a3795d4f956cd16bb75
MoustafaC <moustafa.chamli@gmail.com>
2016-01-28 00:49:15 -0500

    shift on pause

--- godot/scripts/pause.gd
+++ godot/scripts/pause.gd
@@ -2,21 +2,25 @@ extends Spatial
 
 var paused = false
 onready var t = get_node("timer")
+onready var root = get_node("/root/")
+onready var az = root.get_node("scene/player")
 
-func _input(event):
+func _input(ev):
 	var wait = 2
 	var timer = t.set_wait_time(wait)
 	
-	if paused == false and event.is_action_pressed("pause") && !event.is_echo():
+	var pause = ev.is_action_pressed("pause") && !ev.is_echo()
+	
+	if paused == false and pause:
 		_on_pause()
 		paused = true
-	elif paused == true and event.is_action_pressed("pause") && !event.is_echo():
+	elif paused == true and pause:
 		_on_unpause()
 		paused = false
 			
-	if event.is_action("pause") && event.is_pressed():
+	if ev.is_action("pause") && ev.is_pressed():
 		t.start()
-	elif event.is_action("pause") && !event.is_pressed():
+	elif ev.is_action("pause") && !ev.is_pressed():
 		t.stop()
 	else: 
 		timer
@@ -25,10 +29,12 @@ func _on_pause():
 	get_node("pause_menu").set_exclusive(true)
 	get_node("pause_menu").popup()
 	get_tree().set_pause(true)
+	az.set_hidden(true)
 
 func _on_unpause():
 	get_node("pause_menu").hide()
 	get_tree().set_pause(false)
+	az.set_hidden(false)
 
 func _on_timer_timeout():
 	OS.get_main_loop().quit()
